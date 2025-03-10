6995e521f22b896c37d70f3861cf0f2effa6b75f
VieMark <avmarkova2000@gmail.com>
2025-03-10 17:22:20 +0300

    дз 3 сделано git add .

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace HW_2_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // дз 3
+        // 1
+        Console.WriteLine("Простой калькулятор");
+        Console.WriteLine("Введите первое число :");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число :");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int sum = a+b;
+        Console.WriteLine(sum);
+        // 2 
+        Console.WriteLine("Игра угадай число");
+        bool flag = true;
+        int x = 12;
+        while (flag){
+            Console.WriteLine("Введите число :");
+            int y = Convert.ToInt32(Console.ReadLine());
+            if (y == x){
+                Console.WriteLine("Вы угадали !");
+                flag = false;
+            }
+            else if(y < x){
+                Console.WriteLine("Попробуйте увеличить число !");
+            }
+            else if(y>x){
+                Console.WriteLine("Попробуйте уменьшить число !");
+            }
+        }
+
+    }
+}
